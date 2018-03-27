002a62d48fd73766d5deb8b6c9e5add8162490ba
PerHN123 <PerNielsen123@gmail.com>
2018-03-27 09:31:08 +0200

    Notifications
    Sends notification when application goes into backgroud

--- Application/app/src/main/java/d803/busplanning/MainActivity.kt
+++ Application/app/src/main/java/d803/busplanning/MainActivity.kt
@@ -12,9 +12,19 @@ import android.os.AsyncTask.execute
 import java.io.File
 import android.support.v4.app.ActivityCompat
 import android.Manifest
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import java.util.Timer
 import android.content.pm.PackageManager
+import android.support.v4.app.NotificationCompat
 import android.support.v4.content.ContextCompat
-
+import android.app.NotificationChannel
+import android.graphics.Color
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,7 +34,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_CALENDAR),1)
+        ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.READ_CALENDAR,Manifest.permission.ACCESS_NOTIFICATION_POLICY),1)
         var tripButton = findViewById<Button>(R.id.button)
 
 
@@ -49,6 +59,34 @@ class MainActivity : AppCompatActivity() {
         }.start()
 
     }
+
+    override fun onStop() {
+        super.onStop()
+        var title = "hej"
+        val fixedRateTimer = fixedRateTimer(name="kappa",initialDelay = 100, period = 100){
+            sendNotification(title,"Body")
+            title = title +"j"
+        }
+
+
+
+        }
+    fun sendNotification(title: String, body: String){
+        val intent = Intent()
+        val pendingIntent = PendingIntent.getActivity(this@MainActivity,0,intent,0)
+        val notification = Notification.Builder(this@MainActivity)
+                .setTicker("")
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.notification_template_icon_bg)
+                .setContentIntent(pendingIntent).notification
+        notification.flags =Notification.FLAG_AUTO_CANCEL
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0,notification)
+
+    }
+
+
     /** base url http://xmlopen.rejseplanen.dk/bin/rest.exe
     url format
     http://<baseurl>/trip?originId=8600626&destCoordX=<xInteger>&
@@ -60,4 +98,4 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-private operator fun Int.invoke(function: () -> Unit) {}
+
