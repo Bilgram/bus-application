412e363135d34cc2fa2b6350a97d57358ed72fb6
PerHN123 <PerNielsen123@gmail.com>
2018-04-12 14:48:30 +0200

    lol
    kappa

--- Application/app/src/main/java/JSON/JSON.kt
+++ Application/app/src/main/java/JSON/JSON.kt
@@ -1,5 +1,6 @@
 package JSON
-
+import java.text.SimpleDateFormat
+import java.util.Date;
 
 data class TripClass(
 		val TripList: TripList
@@ -14,7 +15,15 @@ data class TripList(
 
 data class Trip(
 		val Leg: List<Leg>
-)
+) {
+	fun getDuration(): Long {
+		val seconds = 1000
+		val to = SimpleDateFormat("hh:mm").parse(Leg.last().Destination.time)
+		val from = SimpleDateFormat("hh:mm").parse(Leg.first().Origin.time)
+		val result = to.time - from.time
+		return result/seconds
+	}
+}
 
 data class Leg(
 		val name: String,
