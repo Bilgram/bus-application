e50a1a60d623b58e5f632ccc44c86db899a5c203
Bilgram <rasmusbilgram94@hotmail.com>
2018-04-30 11:49:59 +0200

    can now do trip.. i think

--- Application/app/src/main/java/JSON/JSON.kt
+++ Application/app/src/main/java/JSON/JSON.kt
@@ -1,53 +1,54 @@
 package JSON
+
 import java.text.SimpleDateFormat
 import java.util.Date;
 
 data class TripClass(
-		val TripList: TripList
+        val TripList: TripList
 )
-
 
 
 data class TripList(
-		val noNamespaceSchemaLocation: String,
-		val Trip: List<Trip>
+        val noNamespaceSchemaLocation: String,
+        val Trip: List<Trip>
 )
 
 data class Trip(
-		val Leg: List<Leg>
+        var Leg: List<Leg>
 ) {
-	fun getDuration(): Long {
-		val seconds = 1000
-		val to = SimpleDateFormat("hh:mm").parse(Leg.last().Destination.time)
-		val from = SimpleDateFormat("hh:mm").parse(Leg.first().Origin.time)
-		val result = to.time - from.time
-		return result/seconds
-	}
+    fun getDuration(): Long {
+        val seconds = 1000
+        val to = SimpleDateFormat("HH:mm").parse(Leg.last().Destination.time)
+        val from = SimpleDateFormat("HH:mm").parse(Leg.first().Origin.time)
+        val result = to.time - from.time
+        return result / seconds
+    }
+
 }
 
 data class Leg(
-		val name: String,
-		val type: String,
-		val Origin: Origin,
-		val Destination: Destination,
-		val Notes: Notes
+        val name: String,
+        val type: String,
+        val Origin: Origin,
+        val Destination: Destination,
+        val Notes: Notes
 )
 
 data class Destination(
-		val name: String,
-		val type: String,
-		val time: String,
-		val date: String
+        val name: String,
+        val type: String,
+        val time: String,
+        val date: String
 
 )
 
 data class Origin(
-		val name: String,
-		val type: String,
-		val time: String,
-		val date: String
+        val name: String,
+        val type: String,
+        val time: String,
+        val date: String
 )
 
 data class Notes(
-		val text: String
+        val text: String
 )
--- Application/app/src/main/java/d803/busplanning/MainActivity.kt
+++ Application/app/src/main/java/d803/busplanning/MainActivity.kt
@@ -1,5 +1,6 @@
 package d803.busplanning
 
+import JSON.Leg
 import JSON.Trip
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -24,6 +25,8 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
+import android.os.Handler
+import android.text.TextUtils.isEmpty
 import android.view.View
 import com.beust.klaxon.Klaxon
 import com.google.android.gms.common.ConnectionResult
@@ -31,6 +34,7 @@ import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.ActivityRecognition
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.NonCancellable.cancel
 import kotlinx.coroutines.experimental.android.UI
 import org.jetbrains.anko.coroutines.experimental.asReference
 import org.jetbrains.anko.doAsync
@@ -40,6 +44,7 @@ import org.w3c.dom.Text
 import java.util.*
 import kotlin.concurrent.fixedRateTimer
 import java.lang.Long.MAX_VALUE
+import java.lang.Runnable
 import java.text.SimpleDateFormat
 import kotlin.concurrent.thread
 
@@ -56,10 +61,7 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, G
 
         val progressBar = this.progressOverview
         updateProgressBar(progressBar)
-        val ref = this.asReference()
-        val field = this.location
         asyncAPICalls()
-
 
         mApiClient = GoogleApiClient.Builder(this)
                 .addApi(ActivityRecognition.API)
@@ -73,236 +75,212 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, G
         val firstUpdate = async(CommonPool) {
             calculatePath()
         }
-        val runningTrip = async(UI) {
-            updateUI(firstUpdate.await())
-        }
         launch(UI) {
-            doTrip(runningTrip.await())
+            updateUI(firstUpdate.await())
+            doTrip(firstUpdate.await())
         }
     }
 
-    private fun updateUI(trip: Trip?): Trip? {
-        val locationField = this.location
-        var fifteenMinNotifcation = false
-        var zeroMinNotfication = false
-        var commitedTrip: Trip? = null
-        val timeField = this.time
-        val busField = this.bus
-        val activityField = this.activity
-        var from = SimpleDateFormat("hh:mm").parse(trip!!.Leg.first().Origin.time)
-        activityField.setText(trip.Leg.first().name)
-        var bus = trip.Leg.filter { l -> l.type != "WALK" }.first()
-
-        busField.setText(bus.name)
-        val fixedRateTimer = fixedRateTimer(name = "UpdateUI", initialDelay = 100, period = 100) {
-            if (commitedTrip == null) {
-                var minutesToBus = (from.time - getCurrentTime()) / 60000
-                runOnUiThread {
-                    timeField.setText(minutesToBus.toString())
-                    locationField.setText(trip.Leg.first().Destination.name)
-                }
-                if (minutesToBus <= 15 && !fifteenMinNotifcation) {
-                    fifteenMinNotifcation = true
-                    sendNotification("gå", "om 15 min")
-                    commitedTrip = trip
-                }
-            } else {
-                var minutesToBus = (from.time - getCurrentTime()) / 60000
-                runOnUiThread { timeField.setText(minutesToBus.toString()) }
-                if (minutesToBus <= 0 && !zeroMinNotfication) {
-                    zeroMinNotfication = true
-                    sendNotification("gå", "nu")
-                    cancel()
-                }
-            }
-        }
-        fixedRateTimer.run {}
-
-        return commitedTrip
+    private fun updateTime(time: Long) {
+        this.time.setText(time.toString())
     }
 
-    override fun onConnected(bundle: Bundle?) {
-        val intent = Intent(this, ActivityDetection::class.java)
-        val pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        val client = ActivityRecognition.getClient(this)
-        client.requestActivityUpdates(0, pendingIntent)
-    }
-
-    override fun onConnectionSuspended(i: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onConnectionFailed(connectionResult: ConnectionResult) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun calculatePath(): Trip? {
-        val customLocation = "Aalborg Busterminal"
-        val destCordinates = getXYCordinates(customLocation)
-        var adress = ""
-        try {
-            val startLocation = locationManager?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-            val geoCoder = Geocoder(this, Locale.getDefault())
-            adress = geoCoder.getFromLocation(startLocation!!.latitude, startLocation.longitude, 1)[0].getAddressLine(0)
-        } catch (ex: SecurityException) {
-            Log.d("myTag", "Security Exception, no location available");
-        }
-        val startCordinates = getXYCordinates(adress)
-        //result mangler time og date og så den søger efter arrival time todo når vi kan læse fra kalender
-        val result = URL("http://xmlopen.rejseplanen.dk/bin/rest.exe/trip?" +
-                "originCoordName=" + adress.toString() + "&originCoordX=" + startCordinates[0] + "&originCoordY=" + startCordinates[1] +
-                "&destCoordName=" + customLocation + "&destCoordX=" + destCordinates[0] + "&destCoordY=" + destCordinates[1] + "&format=json\n").readText()
-        val tripInfo = extractTripInfo(result)
-        return tripInfo
-    }
-
-    private fun getCurrentTime(): Long {
-        val current = java.util.Calendar.getInstance().getTime()
-        var currentHourMin = ""
-        if (current.minutes < 10) {
-            currentHourMin = current.hours.toString() + ":" + "0" + current.minutes.toString()
+    private fun updateUI(trip: Trip?) {
+        this.activity.setText(trip!!.Leg.first().name)
+        if ((trip.Leg.first().type == "WALK") && (trip.Leg.size == 1)) {
+            this.bus.setText(" ")
         } else {
-            currentHourMin = current.hours.toString() + ":" + current.minutes.toString()
+            val bus = trip.Leg.filter { l -> l.type != "WALK" }
+            this.bus.setText(bus.first().name)
         }
-        val to = SimpleDateFormat("hh:mm").parse(currentHourMin)
-        return to.time
+        this.location.setText(trip.Leg.first().Destination.name)
+    }
+
+    private fun getTime(leg: Leg?): Long {
+        val from: Date = SimpleDateFormat("HH:mm").parse(leg!!.Origin.time)
+        return (from.time - getCurrentTime()) / 60000
     }
 
     private fun doTrip(trip: Trip?) {
-        val locationField = this.location
-        val timeField = this.time
-        val busField = this.bus
-        val bus = trip!!.Leg.filter { l -> l.type != "WALK" }.first()
-        busField.setText(bus.name)
-        val fixedRateTimer = fixedRateTimer(name = "UpdateUI", initialDelay = 100, period = 100) {
-            if (trip.Leg.isEmpty()) {
-                cancel()
-            } else {
-
-                var from = SimpleDateFormat("hh:mm").parse(trip.Leg.first().Origin.time)
-                var minutesToNext = (from.time - getCurrentTime()) / 60000
-                runOnUiThread {
-                    timeField.setText(minutesToNext.toString())
-                    locationField.setText(trip.Leg.first().name + "\n " + trip.Leg.first().Destination.name)
-                }
-                if (minutesToNext >= 0) {
-                    trip.Leg.drop(1)
-                }
-
-            }
-        }.run {}
-    }
-
-    private fun extractTripInfo(pathInfo: String): Trip? {
-        var bestTime = MAX_VALUE
-        val reader = Klaxon().parse<TripClass>(pathInfo)
-        val tripMetrics = "first"
-        if (reader != null) {
-            val triplist = reader.TripList
-            val trips = triplist.Trip
-            val tripping = trips.first().Leg
-            var bestTrip = trips.first()
-
-            if (tripMetrics == "first") {
-                return bestTrip
-            } else if (tripMetrics == "fastest") {
-                for (trip in trips) {
-                    if (trip.getDuration() < bestTime) {
-                        bestTime = trip.getDuration()
-                        bestTrip = trip
+        var tripTime = trip!!.getDuration()
+        launch(UI) {
+            for (i in tripTime downTo 0) {
+                val time = getTime(trip.Leg.first())
+                updateTime(time)
+                if (time <= 150) {
+                    // giver tom trip ved sidste element
+                    trip.Leg = trip.Leg.drop(1)
+                    if (trip.Leg.isEmpty()) {
+                        break
                     }
+                    updateUI(trip)
                 }
+                delay(600)
             }
-            return bestTrip
+            cancel()
         }
-        return null
     }
 
-    private fun getXYCordinates(place: String): List<String> {
-        val result = URL("http://xmlopen.rejseplanen.dk/bin/rest.exe/location?input=" + place + "&format=json").readText()
-        val reader = JSONObject(result)
-        val locationlist = reader.getJSONObject("LocationList")
-        var keys = locationlist.keys()
-        //rigtig grim måde at gøre det på måske
-        keys.next()//nonamespaceshemalocation
-        val str = keys.next()//coordlocation eller stoplocation
-        val test = locationlist.get(str)
-        if (test is JSONArray) {
-            val coordLocation = locationlist.getJSONArray(str)
-            val getResult = coordLocation.getJSONObject(0)
-            val x = getResult.getString("x")
-            val y = getResult.getString("y")
-            return listOf<String>(x, y)
+
+
+override fun onConnected(bundle: Bundle?) {
+    val intent = Intent(this, ActivityDetection::class.java)
+    val pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    val client = ActivityRecognition.getClient(this)
+    client.requestActivityUpdates(0, pendingIntent)
+}
+
+override fun onConnectionSuspended(i: Int) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+override fun onConnectionFailed(connectionResult: ConnectionResult) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+private fun calculatePath(): Trip? {
+    val customLocation = "Aalborg busterminal"
+    val destCordinates = getXYCordinates(customLocation)
+    var address = ""
+    try {
+        val startLocation = locationManager?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        val geoCoder = Geocoder(this, Locale.getDefault())
+        address = geoCoder.getFromLocation(startLocation!!.latitude, startLocation.longitude, 1)[0].getAddressLine(0)
+    } catch (ex: SecurityException) {
+        address = "Selma Lagerløfsvej 23"
+        Log.d("myTag", "Security Exception, no location available");
+    }
+    val startCordinates = getXYCordinates(address)
+    //result mangler time og date og så den søger efter arrival time todo når vi kan læse fra kalender
+    val result = URL("http://xmlopen.rejseplanen.dk/bin/rest.exe/trip?" +
+            "originCoordName=" + address.toString() + "&originCoordX=" + startCordinates[0] + "&originCoordY=" + startCordinates[1] +
+            "&destCoordName=" + customLocation + "&destCoordX=" + destCordinates[0] + "&destCoordY=" + destCordinates[1] + "&format=json\n").readText()
+    val tripInfo = extractTripInfo(result)
+    return tripInfo
+}
+
+private fun getCurrentTime(): Long {
+    val current = java.util.Calendar.getInstance().getTime()
+    var currentHourMin = ""
+    if (current.minutes < 10) {
+        currentHourMin = current.hours.toString() + ":" + "0" + current.minutes.toString()
+    } else {
+        currentHourMin = current.hours.toString() + ":" + current.minutes.toString()
+    }
+    val to = SimpleDateFormat("hh:mm").parse(currentHourMin)
+    return to.time
+}
+
+
+private fun extractTripInfo(pathInfo: String): Trip? {
+    var bestTime = MAX_VALUE
+    val reader = Klaxon().parse<TripClass>(pathInfo)
+    val tripMetrics = "first"
+    if (reader != null) {
+        val triplist = reader.TripList
+        val trips = triplist.Trip
+        var bestTrip = trips.first()
+
+        if (tripMetrics == "first") {
+            return bestTrip
+        } else if (tripMetrics == "fastest") {
+            for (trip in trips) {
+                if (trip.getDuration() < bestTime) {
+                    bestTime = trip.getDuration()
+                    bestTrip = trip
+                }
+            }
         }
-        val getResult = locationlist.getJSONObject(str)
+        return bestTrip
+    }
+    return null
+}
+
+private fun getXYCordinates(place: String): List<String> {
+    val result = URL("http://xmlopen.rejseplanen.dk/bin/rest.exe/location?input=" + place + "&format=json").readText()
+    val reader = JSONObject(result)
+    val locationlist = reader.getJSONObject("LocationList")
+    var keys = locationlist.keys()
+    //rigtig grim måde at gøre det på måske
+    keys.next()//nonamespaceshemalocation
+    val str = keys.next()//coordlocation eller stoplocation
+    val test = locationlist.get(str)
+    if (test is JSONArray) {
+        val coordLocation = locationlist.getJSONArray(str)
+        val getResult = coordLocation.getJSONObject(0)
         val x = getResult.getString("x")
         val y = getResult.getString("y")
         return listOf<String>(x, y)
     }
+    val getResult = locationlist.getJSONObject(str)
+    val x = getResult.getString("x")
+    val y = getResult.getString("y")
+    return listOf<String>(x, y)
+}
 
-    private fun getLocation() {
-        if (locationManager == null)
-            locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        try {
-            // Request location updates
-            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 10f, locationListener);
-        } catch (ex: SecurityException) {
-            Log.d("myTag", "Security Exception, no location available");
-        }
+private fun getLocation() {
+    if (locationManager == null)
+        locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    try {
+        // Request location updates
+        locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 10f, locationListener);
+    } catch (ex: SecurityException) {
+        Log.d("myTag", "Security Exception, no location available");
+    }
+}
+
+override fun onStop() {
+    super.onStop()
+    var title = "hej"
+    val fixedRateTimer = fixedRateTimer(name = "kappa", initialDelay = 100, period = 100) {
+        //sendNotification(title,"Body")
+        title = title + "j"
+    }
+}
+
+fun sendNotification(title: String, body: String) {
+    val intent = Intent()
+    val pendingIntent = PendingIntent.getActivity(this@MainActivity, 0, intent, 0)
+    val notification = Notification.Builder(this@MainActivity) //https://stackoverflow.com/questions/45462666/notificationcompat-builder-deprecated-in-android-o?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+            .setTicker("")
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(R.drawable.notification_template_icon_bg)
+            .setContentIntent(pendingIntent).notification
+    notification.flags = Notification.FLAG_AUTO_CANCEL
+    val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    notificationManager.notify(0, notification)
+}
+
+fun updateProgressBar(progressBar: ProgressBar) {
+    runOnUiThread {
+        val drawable = progressBar.progressDrawable
+        drawable.colorFilter = createColor()
+        progressBar.setProgressDrawable(drawable)
+        progressBar.progress = 50
+    }
+}
+
+fun createColor(): PorterDuffColorFilter {
+    val color = android.graphics.Color.argb(255, 153, 153, 153)
+    val colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.MULTIPLY)
+    return colorFilter
+}
+
+private val locationListener: LocationListener = object : LocationListener {
+    override fun onLocationChanged(location: Location) {
+        sendNotification(location.latitude.toString(), location.toString())
     }
 
-    override fun onStop() {
-        super.onStop()
-        var title = "hej"
-        val fixedRateTimer = fixedRateTimer(name = "kappa", initialDelay = 100, period = 100) {
-            //sendNotification(title,"Body")
-            title = title + "j"
-        }
-    }
-
-    fun sendNotification(title: String, body: String) {
-        val intent = Intent()
-        val pendingIntent = PendingIntent.getActivity(this@MainActivity, 0, intent, 0)
-        val notification = Notification.Builder(this@MainActivity) //https://stackoverflow.com/questions/45462666/notificationcompat-builder-deprecated-in-android-o?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-                .setTicker("")
-                .setContentTitle(title)
-                .setContentText(body)
-                .setSmallIcon(R.drawable.notification_template_icon_bg)
-                .setContentIntent(pendingIntent).notification
-        notification.flags = Notification.FLAG_AUTO_CANCEL
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, notification)
-    }
-
-    fun updateProgressBar(progressBar: ProgressBar) {
-        runOnUiThread {
-            val drawable = progressBar.progressDrawable
-            drawable.colorFilter = createColor()
-            progressBar.setProgressDrawable(drawable)
-            progressBar.progress = 50
-        }
-    }
-
-    fun createColor(): PorterDuffColorFilter {
-        val color = android.graphics.Color.argb(255, 153, 153, 153)
-        val colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.MULTIPLY)
-        return colorFilter
-    }
-
-    private val locationListener: LocationListener = object : LocationListener {
-        override fun onLocationChanged(location: Location) {
-            sendNotification(location.latitude.toString(), location.toString())
-        }
-
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-        override fun onProviderEnabled(provider: String) {}
-        override fun onProviderDisabled(provider: String) {}
-    }
-    /** base url http://xmlopen.rejseplanen.dk/bin/rest.exe
-    url format
-    http://<baseurl>/trip?originId=8600626&destCoordX=<xInteger>&
-    destCoordY=<yInteger>&destCoordName=<NameOfDestination>&date=
-    19.09.10&time=07:02&useBus=0*/
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+    override fun onProviderEnabled(provider: String) {}
+    override fun onProviderDisabled(provider: String) {}
+}
+/** base url http://xmlopen.rejseplanen.dk/bin/rest.exe
+url format
+http://<baseurl>/trip?originId=8600626&destCoordX=<xInteger>&
+destCoordY=<yInteger>&destCoordName=<NameOfDestination>&date=
+19.09.10&time=07:02&useBus=0*/
 }
 
 
